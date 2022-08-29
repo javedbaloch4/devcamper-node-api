5ff40907c7604bd2693e346b2dee31843df8cbf6
javedbaloch4 <mjavedahmed@live.com>
2022-08-30 04:05:29 +0500

    Morgan middleware added

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import bootcamps from "./routes/bootcamp.js";
-import { logger } from "./middleware/logger.js";
+import logger from "morgan";
+import morgan from "morgan";
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.use(logger);
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // Mount routes
 app.use("/api/v1/bootcamps", bootcamps);
