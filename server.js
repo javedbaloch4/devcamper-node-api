d39ca457b3afa5504d8b019ee28013ce0f793942
javedbaloch4 <mjavedahmed@live.com>
2022-08-30 02:52:27 +0500

    Creating basic routes

--- server.js
+++ server.js
@@ -6,6 +6,31 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.get("/api/v1/bootcamp", (req, res) => {
+  res.status(200).json({ success: true, message: "Show all the bootcamps" });
+});
+
+app.get("/api/v1/bootcamp/:id", (req, res) => {
+  const { id } = req.params;
+  res.status(200).json({ success: true, message: `Show ing ${id} bootcamp` });
+});
+
+app.post("/api/v1/bootcamp", (req, res) => {
+  res
+    .status(201)
+    .json({ success: true, message: "Bootcamp has been created " });
+});
+
+app.put("/api/v1/bootcamp/:id", (req, res) => {
+  const { id } = req.params;
+  res.status(200).json({ success: true, message: `Updated ${id} bootcamp` });
+});
+
+app.delete("/api/v1/bootcamp/:id", (req, res) => {
+  const { params } = req.params;
+  res.status(200).json({ success: true, message: `Deleted ${id} bootcamp` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
