3995de72030d4623892219ff31f7076c1b6427d2
javedbaloch4 <mjavedahmed@live.com>
2022-09-19 02:28:55 +0500

    Pagination

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -16,9 +16,9 @@ export const getBootcamps = asyncHandler( async (req, res, next) => {
   const reqQuery = { ...req.query }
 
   // Remove fields
-  const removeFields = ['select']
+  const removeFields = ['select','sort','page','limit']
 
-  // Loop to remove fields
+  // Loop to remove fields 
   removeFields.forEach(param => delete reqQuery[param])
 
   // Stringfy req.query
@@ -45,10 +45,35 @@ export const getBootcamps = asyncHandler( async (req, res, next) => {
     query = query.sort('-createdAt')
   }
 
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1
+  const limit = parseInt(req.query.limit, 10) || 25
+  const startIndex = (page - 1) * limit
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+  
+  query = query.skip(startIndex).limit(limit)
+  
   // Execute the query
   const bootcamps = await query;
 
-  res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
+  // Paginate result
+  const pagination = {}
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    }
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    }
+  }
+
+  res.status(200).json({ success: true, count: bootcamps.length, pagination: pagination, data: bootcamps });
 });
 
 /**
